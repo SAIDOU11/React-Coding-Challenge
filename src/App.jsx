a7c43e9b98c96486923e202c60c5121d3cf5b021
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-09-11 05:43:20 +0200

    challenge 2/40

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
 const App = () => {
-  return <h1>App</h1>;
+  const [flashCard, setFlashCard] = useState({
+    question: 'What is React?',
+    choices: ['A JavaScript framework', 'A JavaScript library'],
+    answer: 'A JavaScript library.',
+    explanation: `It's absolutely crucial that if you ever hear anyone have the
+              audacity to call it a framework, you must correct them as
+              pedantically as possible, preferably starting your response with
+              the phrase "um, actually..."`,
+  });
+
+  return (
+    <div>
+      <header>
+        <img
+          src="./assets/react.svg
+        "
+        />
+        <h1> React Study Buddy </h1>
+      </header>
+
+      {/*-------Edit the div below!------------*/}
+
+      <div className="flash-card ">
+        {/*-------Edit the div above!------------*/}
+
+        <div className="flash-card-inner">
+          <div className="flash-card-front">
+            <p className="question">{flashCard.question}</p>
+            <ol type="a">
+              {flashCard.choices.map((choice) => (
+                <li key={nanoid()}>{choice}</li>
+              ))}
+            </ol>
+          </div>
+          <div className="flash-card-back">
+            <p className="answer">{flashCard.answer}</p>
+            <p>{flashCard.explanation}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
